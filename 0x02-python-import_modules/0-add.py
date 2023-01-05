12a7516107321ad219625c8292cc39fbd1158f99
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-01-05 20:53:40 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+  
   from add_0 import add
 
   a = 1
   b = 2
-  c = add(a, b)
-  print("{a} + {b} = {c}".format(a, b, c))
+  
+  print("{0} + {1} = {2}".format(a, b, add(a,b))
