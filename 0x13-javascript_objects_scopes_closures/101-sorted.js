5722e6766b7315fda79511ae65ffb5e7f7169e9a
ygbadamosi662 <gbadamosiy662@gmail.com>
2023-04-13 02:48:33 -0700

    corrected

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,15 +3,15 @@ const dict = require('./101-data.js').dict;
 const list = [];
 const newDict = {};
 
-Object.keys(dict).foreach((key) => {
+Object.keys(dict).forEach((key) => {
   if (!list.includes(dict[key])) {
     list.push(dict[key]);
     newDict[dict[key]] = [];
   }
 });
 
-Object.keys(dict).foreach((key) => {
-  list.foreach((app) => {
+Object.keys(dict).forEach((key) => {
+  list.forEach((app) => {
     if (dict[key] === app) {
       newDict[dict[key]].push(key);
     }
