8e920c280ac2e7df5e6ce3ae98e24e95b8da7d8a
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-04-13 11:06:27 +0100

    Create 102-concat.js

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const contentA = fs.readFile(process.argv[2]);
+const contentB = fs.readFile(process.argv[3]);
+const contentC = Buffer.concat([contentA, contentB]);
+fs.writeFile(process.argv[4], contentC);
