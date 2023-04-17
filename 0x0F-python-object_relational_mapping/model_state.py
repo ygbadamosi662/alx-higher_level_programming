07b1b9f21356d09dc6f1bb4f575830b01dfc16f5
ygbadamosi662 <gbadamosiy662@gmail.com>
2023-04-17 12:32:44 -0700

    statrs

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Defines a class State an intance of Base = declarative_base()
+"""
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import sessionmaker
+
+if __name__ == "__main__":
+    url = 'mysql+pymysql://<username>:<password>@localhost:'
+    url = url + '3306/<database_name>'
+    engine = create_engine(url)
+    Base = declarative_base()
+
+    class State(Base):
+        __tablename__ = 'states'
+
+        id = Column(Integer, primary_key=True, nullable=False)
+        name = Column(String(128), nullable=False)
+
+        Base.metadate.create_all(engine)
