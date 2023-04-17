35d74df93192b7298d21b97bde9d0e5f46481b10
ygbadamosi662 <gbadamosiy662@gmail.com>
2023-04-17 13:26:59 -0700

    starts

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
 """
-Defines a class State an intance of Base = declarative_base()
+Defines a class State an intance of Base = declarative_base
 """
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, Integer, String
 
-if __name__ == "__main__":
-    Base = declarative_base()
+Base = declarative_base()
 
-    class State(Base):
-        """
-        Defines a class State
-        """
-        __tablename__ = 'states'
 
-        id = Column(Integer, primary_key=True, nullable=False,
-                    autoincrement=True)
-        name = Column(String(128), nullable=False)
+class State(Base):
 
-    url = 'mysql+mysqldb://root:password@localhost:3306/hbtn_0e_6_usa'
-    engine = create_engine(url)
+    """
+    Defines a class State
+    """
+    __tablename__ = 'states'
 
-    Base.metadata.create_all(engine)
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
+url = 'mysql+mysqldb://root:password@localhost:3306/hbtn_0e_6_usa'
+
+engine = create_engine(url)
+
+Base.metadata.create_all(engine)
