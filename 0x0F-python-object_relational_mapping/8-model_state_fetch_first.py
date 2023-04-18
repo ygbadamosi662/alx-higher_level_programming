a47b40977395049946729e797b2bbaacc55521db
ygbadamosi662 <gbadamosiy662@gmail.com>
2023-04-18 07:59:57 -0700

    okay

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,16 +13,16 @@ if __name__ == "__main__":
     url = "mysql+mysqldb://{}:{}@localhost:3306/{}"
     url = url.format(sys.argv[1], sys.argv[2], sys.argv[3])
 
-    engine = create_engine(url)
+    engine = create_engine(url, pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    count = session.query(my_table).count()
+    first_state = session.query(State).order_by(State.id).first()
 
-    if count > 0:
-        state = session.query(State).first()
-        print("{}: {}".format(state.id, state.name))
-
-    if count == 0:
-        print("Nothing\n")
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
