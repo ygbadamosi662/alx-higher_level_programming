81d444c7bcb0d575d307f2346ede09135808f1a9
ygbadamosi662 <gbadamosiy662@gmail.com>
2023-04-18 09:27:14 -0700

    okay

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Adds a state instance to hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    url = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    url = url.format(sys.argv[1], sys.argv[2], sys.argv[3])
+
+    engine = create_engine(url)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    state = session.query(State).filter_by(name='Louisiana').first()
+
+    print(state.id)
