a098d37c502c837809a47aa8b453b3c017dc4c78
ygbadamosi662 <gbadamosiy662@gmail.com>
2023-04-18 09:07:33 -0700

    okay

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -24,6 +24,6 @@ if __name__ == "__main__":
     state = session.query(State).filter(State.name == sys.argv[4]).first()
 
     if state:
-        print("{}: {}".format(state.id, state.name))
+        print(state.id)
     else:
         print("Not found")
