0bddb546915decf24f5542533f77c27484b36ea5
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-04-27 21:15:27 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# Display the byte size of the HTTP response header for a given URL.
-
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 URL"
-  exit 1
-fi
-
-response=$(curl -s -w "%{size_download})" -o /dev/null $1)
-
-echo $response
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
