a602d59434cb3622f9895dda26c6e6f284d91c72
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-04-27 22:02:03 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # Get the response body for a given URL for 200 status code responses.
-
-response_file=$(mktemp)
-response=$(curl -s -w "%{http_code}" -o "$response_file" "$1")
-status_code=$(echo "$response" | awk '{print $2}')
-if [ "$status_code" = "200" ]; then
-  response_body=$(cat "$response_file")
-  echo "$response_body"
-fi
-
-rm "$response_file"
+curl -sL "$1"
