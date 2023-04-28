49c957d63c2e412ac113258c321419f41a378e4b
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-04-28 08:50:27 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a peak-finding algorithm."""
 
+
 def find_peak(nums):
     """ Finds the peak in a list of integers """
     if not nums:
@@ -10,7 +11,7 @@ def find_peak(nums):
     
     while left < right:
         mid = (left + right) // 2
-        
+
         if nums[mid] < nums[mid + 1]:
             left = mid + 1
         else:
