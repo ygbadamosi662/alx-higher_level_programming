46dee62b682ee07ce97b5aedcfec019649bb09f1
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-04-28 22:17:54 +0100

    Create 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status. and displays the body"""
+import requests
+
+
+if __name__ == "__main__":
+
+    res = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
