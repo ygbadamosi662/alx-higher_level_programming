3e83fc5a46b196cdd7d87c5d7bba8c067fea81d7
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-04-28 22:21:16 +0100

    Create 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    res = requests.get(sys.argv[1])
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
