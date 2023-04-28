a0857c86a9bd13e901cc01334d80701bfe7b5a3e
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-04-28 08:26:19 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,8 +7,10 @@ def find_peak(nums):
         return None
 
     left, right = 0, len(nums) - 1
+    
     while left < right:
         mid = (left + right) // 2
+        
         if nums[mid] < nums[mid + 1]:
             left = mid + 1
         else:
