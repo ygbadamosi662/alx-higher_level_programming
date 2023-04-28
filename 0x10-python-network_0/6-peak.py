fc78e101c851a04430f3c0c5d118c35c2932fff4
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-04-28 08:24:55 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
 """Defines a peak-finding algorithm."""
 
-
-def find_peak(list_of_integers):
+def find_peak(nums):
     """ Finds the peak in a list of integers """
-    if list_of_integers == []:
+    if not nums:
         return None
 
-    length = len(list_of_integers)
-    m = int(length / 2)
-    li = list_of_integers
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] < nums[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
 
-    if m - 1 < 0 and m + 1 >= length:
-        return li[m]
-    elif m - 1 < 0:
-        return li[m] if li[m] > li[m + 1] else li[m + 1]
-    elif m + 1 >= length:
-        return li[m] if li[m] > li[m - 1] else li[m - 1]
-
-    if li[m - 1] < li[m] > li[m + 1]:
-        return li[m]
-
-    if li[m + 1] > li[m - 1]:
-        return find_peak(li[m:])
-    return find_peak(li[:m])
+    return nums[left]
